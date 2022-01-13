e09c9b2901b773454391b95400ad7eaad8343208
OnSeMiRo <79685152+OnSe-MiRo@users.noreply.github.com>
2022-01-13 15:35:45 +0900

    Update json_to_yolov5_txt.py

--- json_to_yolov5_txt.py
+++ json_to_yolov5_txt.py
@@ -14,8 +14,9 @@ def json_to_yolov5_txt(json_folder_path,save_txt_folder_path):
         height=data['label_info']['image']['height']
         for i in data['label_info']['annotations']:
             category_id=i['category_id']
-            bbox=np.array(i['bbox'],dtype=np.float32)
-            bbox_to_resize=np.array([1/width,1/height,1/width,1/height],dtype=np.float32)
-            rebbox=bbox*bbox_to_resize
-            new_txt.write(str(category_id)+' '+str(rebbox[0])+' '+str(rebbox[1])+' '+str(rebbox[2]-rebbox[0])+' '+str(rebbox[3]-rebbox[1])+'\n')
+            bbox=np.array(i['bbox'],dtype=np.int)
+            bbox=np.array([bbox[0]+bbox[2], bbox[1]+bbox[3], np.abs(bbox[2]-bbox[0]), np.abs(bbox[3]-bbox[1])], dtype=np.int)
+            bbox_to_resize=np.array([width*2,height*2,width,height],dtype=np.int)
+            rebbox=np.divide(bbox,bbox_to_resize)
+            new_txt.write(str(category_id)+' '+str(rebbox[0])+' '+str(rebbox[1])+' '+str(rebbox[2])+' '+str(rebbox[3])+'\n')
     
